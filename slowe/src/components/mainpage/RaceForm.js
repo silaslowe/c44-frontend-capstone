14a25a59fe312504f3b76b5b87ede1dc0ad31e49
negative1skunk@gmail.com <negative1skunk@gmail.com>
2020-12-08 09:44:33 -0600

    calc refactor

--- slowe/src/components/mainpage/RaceForm.js
+++ slowe/src/components/mainpage/RaceForm.js
@@ -1,18 +1,18 @@
-import React, { useContext, useRef, useEffect } from "react"
+// import React, { useContext, useRef, useEffect } from "react"
 
-export const RaceForm = () => {
-  const { addRace } = useContext(RacesContext)
+// export const RaceForm = () => {
+//   const { addRace } = useContext(RacesContext)
 
-  const name = useRef(null)
-  const distance = useRef(null)
-  const city = useRef(null)
-  const state = useRef(null)
-  const date = useRef(null)
+//   const name = useRef(null)
+//   const distance = useRef(null)
+//   const city = useRef(null)
+//   const state = useRef(null)
+//   const date = useRef(null)
 
-  const constructNewRace = () => {
-    const distanceInt = parseInt(distance.current.value)
-    const distIntMiles = ""
+//   const constructNewRace = () => {
+//     const distanceInt = parseInt(distance.current.value)
+//     const distIntMiles = ""
 
-    addRace()
-  }
+//     addRace()
+//   }
 }
