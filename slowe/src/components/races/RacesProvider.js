f8165c113ebd9c4ef89a473921558b42d6e010ac
negative1skunk@gmail.com <negative1skunk@gmail.com>
2020-12-08 09:56:03 -0600

    started on the race form

---
+++ slowe/src/components/races/RacesProvider.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react"
+
+export const RacesContext = React.createContext()
+
+export const RacesProvider = (props) => {
+  const [races, setRaces] = useState([])
+
+  const getRaces = () => {
+    return fetch("http://localhost:8088/races")
+      .then((res) => res.json())
+      .then(setRaces)
+  }
+
+  const addRace = (race) => {
+    return fetch("http://localhost:8088/races", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(race),
+    }).then(getRaces)
+  }
+
+  const editRace = (race) => {
+    return fetch(`http://localhost:8088/races/${race.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(race),
+    }).then(getRaces)
+  }
+  return (
+    <RacesContext.Provider value={{ races, getRaces, addRace, editRace }}>
+      {props.children}
+    </RacesContext.Provider>
+  )
+}
