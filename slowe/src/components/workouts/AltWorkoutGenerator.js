8fc67fa2cc745fbcd1cf063fa6db3e48534e6a8d
negative1skunk@gmail.com <negative1skunk@gmail.com>
2020-12-19 16:04:41 -0600

    AltRaceGen: removed all useState vars, removed getCurrentWorkout func and currentWorkout func, fixed num of workouts by subtracting 1 from the daysBetween in the for loop. Placeholder: added useEffect and moved routing into that outside the return

--- slowe/src/components/workouts/AltWorkoutGenerator.js
+++ slowe/src/components/workouts/AltWorkoutGenerator.js
@@ -1,25 +1,13 @@
-import React, { useContext, useEffect, useState } from "react"
+import React, { useContext } from "react"
 import { WorkoutContext } from "./WorkoutProvider"
 
 export const AltWorkoutGenerator = (props) => {
-  const { addWorkout, getWorkouts, workouts } = useContext(WorkoutContext)
-  const [generated, setGenerated] = useState(false)
-  const [currentWorkouts, setCurrentWorkouts] = useState([])
-  const [daysNeeded, setDaysNeeded] = useState()
-  const [arrayLength, setArrayLength] = useState("")
+  const { addWorkout } = useContext(WorkoutContext)
+
   // # of days between start of workouts and race
   let daysBetween = ""
   // holds generated workout objects
   let workoutArray = []
-
-  useEffect(() => {
-    getWorkouts()
-  }, [])
-
-  // finds workouts for current race
-  useEffect(() => {
-    setCurrentWorkouts(workouts.filter((workout) => workout.raceId === props.currentRace.id))
-  }, [workouts])
 
   const generator = () => {
     // filters the workouts by the current races
@@ -29,9 +17,8 @@ export const AltWorkoutGenerator = (props) => {
     let startingDate = startDate
     // determines the days between the the start of training and the race date
     daysBetween = Math.ceil((raceDate - startDate) / (24 * 60 * 60 * 1000))
-    setDaysNeeded(daysBetween)
     // if the amount of filtered workouts is less than days between, which is the needed amount, the for loop gernerates a workout card for each day of training and pushed it into the workoutARray.
-    for (let i = 0; i < daysBetween; i++) {
+    for (let i = 0; i < daysBetween - 1; i++) {
       startingDate += day
       workoutArray.push({
         raceId: props.currentRace.id,
@@ -55,7 +42,6 @@ export const AltWorkoutGenerator = (props) => {
     return Promise.all(
       workoutArray.map((workout, i) => setTimeout(() => addWorkout(workout), i * 250))
     ).then(() => {
-      setTimeout(() => setGenerated(true), delay - 50)
       setTimeout(
         () => props.history.push({ pathname: "/placehold", state: { stateChange: 1 } }),
         delay
